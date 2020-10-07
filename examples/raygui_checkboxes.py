cef70786969df58fe693849dd139542786e528e0
Roberto Alsina <roberto.alsina@gmail.com>
2020-10-07 14:08:14 -0300

    Bind raygui because why not

---
+++ examples/raygui_checkboxes.py
@@ -0,0 +1,16 @@
+from cobra_py import rl
+
+sw = 800
+sh = 450
+
+rl.init_window(sw, sh, b"CheckBoxes")
+rl.set_target_fps(60)
+cb = 1
+
+while not rl.window_should_close():
+    rl.begin_drawing()
+    rl.clear_background(rl.RAYWHITE)
+    cb = rl.gui_check_box((300, 300, 20, 20), b"Foo", cb)
+    rl.draw_fps(10, 10)
+    rl.end_drawing()
+rl.close_window()
--- tasks.py
+++ tasks.py
@@ -6,17 +6,23 @@ from invoke import task
 
 @task
 def bind_raylib(c):
-    print("Building CFFI")
+    print("Building raylib CFFI")
     ffi = cffi.FFI()
-    includes = pathlib.Path().absolute() / "includes"
-    h_file = includes / "raylib.h"
+    includes = pathlib.Path().absolute() / "include"
+    h_file = includes / "_raylib.h"
     with open(h_file) as inf:
         ffi.cdef(inf.read())
     ffi.set_source(
         "cobra_py.raylib",
-        '#include "raylib.h"',
+        """
+            #define RAYGUI_IMPLEMENTATION
+            #define RAYGUI_SUPPORT_ICONS
+            #include "raylib.h"
+            #include "raygui.h"
+        """,
         libraries=["raylib"],
         extra_link_args=["-Wl,-rpath,."],
+        include_dirs=[includes],
     )
     ffi.compile()
 
