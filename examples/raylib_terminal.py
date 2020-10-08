7fe884c68a3b38516445107ae65f7935200cdebe
Roberto Alsina <roberto.alsina@gmail.com>
2020-10-08 16:12:14 -0300

    Working terminal example

--- examples/raylib_terminal.py
+++ examples/raylib_terminal.py
@@ -16,9 +16,9 @@ sh = 450
 
 
 rl.init_window(sw, sh, b"CobraPy Terminal!")
-font_path = str(
-    Path(rl.__file__).parent / "resources" / "fonts" / "Kepler-452b.ttf"
-).encode("utf-8")
+font_path = str(Path(rl.__file__).parent / "resources" / "fonts" / "monoid.ttf").encode(
+    "utf-8"
+)
 font = rl.load_font_ex(font_path, 24, ffi.NULL, 0)
 text_size = rl.measure_text_ex(font, b"X", font.baseSize, 0)
 sw = int(80 * text_size.x)
@@ -27,7 +27,7 @@ rl.set_window_size(sw, sh)
 rl.set_target_fps(60)
 
 
-term = pyte.Screen(80, 24)
+term = pyte.HistoryScreen(80, 25)
 stream = pyte.ByteStream(term)
 
 p_pid, master_fd = pty.fork()
@@ -35,10 +35,35 @@ if p_pid == 0:  # Child process
     os.execvpe(
         "bash",
         ["bash"],
-        env=dict(TERM="linux", COLUMNS="80", LINES="24", LC_ALL="en_US.UTF-8"),
+        env=dict(TERM="linux", COLUMNS="80", LINES="25", LC_ALL="en_US.UTF-8"),
     )
 
 p_out = os.fdopen(master_fd, "w+b", 0)
+
+
+def esc(d: bytes):
+    return b"\x1b[" + d
+
+
+keys = {
+    36: b"\n",  # Enter
+    111: esc(b"A"),  # Cursor up
+    116: esc(b"B"),  # Cursor down
+    114: esc(b"C"),  # Cursor right
+    113: esc(b"D"),  # Cursor left
+    22: b"\b",  # Backspace
+}
+
+colors = {
+    "black": rl.BLACK,
+    "red": rl.RED,
+    "green": rl.GREEN,
+    "brown": rl.BROWN,
+    "blue": rl.BLUE,
+    "magenta": rl.MAGENTA,
+    "cyan": rl.SKYBLUE,
+    "white": rl.WHITE,
+}
 
 
 @ffi.callback("void(int,int,int,int)")
@@ -46,24 +71,23 @@ def key_event(key, scancode, action, mods):
     print("scancode=>", key, scancode, action)
     if mods == 1:  # Key release
         return
-    if action == 36:
-        p_out.write(b"\n")
+    if data := keys.get(action):
+        p_out.write(data)
 
 
 rl.set_key_callback(key_event)
 
 
 while not rl.window_should_close():
+    # FIXME: can probably handle it all in key_event
     key = rl.get_key_pressed()
     while key > 0:
         k = chr(key).encode("utf-8")
-        print("===>", k, chr(key), key)
         p_out.write(k)
         key = rl.get_key_pressed()
-        print("===>", k, chr(key), key)
 
     rl.begin_drawing()
-    rl.clear_background(rl.RAYWHITE)
+    rl.clear_background(rl.DARKBLUE)
 
     ready, _, _ = select.select([p_out], [], [], 0)
     if ready:
@@ -71,15 +95,40 @@ while not rl.window_should_close():
         if data:
             stream.feed(data)
 
-    for i, row in enumerate(term.display):
-        text = row.encode("utf-8")
-        rl.draw_text_ex(
-            font,
-            text,
-            (0, i * text_size.y),
-            font.baseSize,
-            0,
-            rl.RED,
-        )
-    # rl.draw_fps(10, 10)
+    for y, line in enumerate(term.display):
+        line = term.buffer[y]
+        for x in range(term.columns):  # Can't enumerate, it's sparse
+            char = line[x]
+
+            fg = colors.get(char.fg, rl.WHITE)
+            bg = colors.get(char.bg, rl.BLACK)
+            if char.reverse:
+                fg, bg = bg, fg
+
+            rl.draw_rectangle(
+                int(x * text_size.x),
+                int(y * text_size.y),
+                int(text_size.x),
+                int(text_size.y),
+                bg,
+            )
+            rl.draw_text_ex(
+                font,
+                char.data.encode("utf-8"),
+                (x * text_size.x, y * text_size.y),
+                font.baseSize,
+                0,
+                fg,
+            )
+    term.dirty.clear()
+
+    # draw cursor
+    rl.draw_rectangle(
+        int(term.cursor.x * text_size.x),
+        int(term.cursor.y * text_size.y),
+        int(text_size.x),
+        int(text_size.y),
+        (100, 108, 100, 100),
+    )
+    rl.draw_fps(400, 50)
     rl.end_drawing()
