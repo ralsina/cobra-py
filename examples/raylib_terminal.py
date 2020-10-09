6ec4355b5fb84acc323784ba9ee86ea8333124da
Roberto Alsina <roberto.alsina@gmail.com>
2020-10-08 21:50:20 -0300

    reorganize code

--- examples/raylib_terminal.py
+++ examples/raylib_terminal.py
@@ -10,37 +10,8 @@ import pyte
 from cobra_py import rl
 from cobra_py.raylib import ffi
 
-sw = 800
-sh = 450
 
-
-rl.init_window(sw, sh, b"CobraPy Terminal!")
-font_path = str(Path(rl.__file__).parent / "resources" / "fonts" / "monoid.ttf").encode(
-    "utf-8"
-)
-font = rl.load_font_ex(font_path, 24, ffi.NULL, 0)
-text_size = rl.measure_text_ex(font, b"X", font.baseSize, 0)
-sw = int(80 * text_size.x)
-sh = int(25 * text_size.y)
-rl.set_window_size(sw, sh)
-rl.set_target_fps(24)
-
-
-term = pyte.HistoryScreen(80, 25)
-stream = pyte.ByteStream(term)
-
-p_pid, master_fd = pty.fork()
-if p_pid == 0:  # Child process
-    os.execvpe(
-        "bash",
-        ["bash"],
-        env=dict(TERM="xterm", COLUMNS="80", LINES="25", LC_ALL="en_US.UTF-8"),
-    )
-
-p_out = os.fdopen(master_fd, "w+b", 0)
-
-
-def esc(d: bytes):
+def _esc(d: bytes):
     return b"\x1b" + d
 
 
@@ -48,24 +19,24 @@ keys = {
     36: b"\n",  # Enter
     23: b"\t",  # Tab
     66: b"\x1b",  # ESC
-    111: esc(b"[A"),  # Cursor up
-    116: esc(b"[B"),  # Cursor down
-    114: esc(b"[C"),  # Cursor right
-    113: esc(b"[D"),  # Cursor left
+    111: _esc(b"[A"),  # Cursor up
+    116: _esc(b"[B"),  # Cursor down
+    114: _esc(b"[C"),  # Cursor right
+    113: _esc(b"[D"),  # Cursor left
     22: b"\b",  # Backspace
-    119: esc(b"[3~"),  # Delete
-    67: esc(b"OP"),  # F1
-    68: esc(b"OQ"),  # F2
-    69: esc(b"OR"),  # F3
-    70: esc(b"OS"),  # F4
-    71: esc(b"[15~"),  # F5
-    72: esc(b"[17~"),  # F6
-    73: esc(b"[18~"),  # F7
-    74: esc(b"[19~"),  # F8
-    75: esc(b"[20~"),  # F9
-    76: esc(b"[21~"),  # F10
-    95: esc(b"[23~"),  # F11
-    96: esc(b"[24~"),  # F12
+    119: _esc(b"[3~"),  # Delete
+    67: _esc(b"OP"),  # F1
+    68: _esc(b"OQ"),  # F2
+    69: _esc(b"OR"),  # F3
+    70: _esc(b"OS"),  # F4
+    71: _esc(b"[15~"),  # F5
+    72: _esc(b"[17~"),  # F6
+    73: _esc(b"[18~"),  # F7
+    74: _esc(b"[19~"),  # F8
+    75: _esc(b"[20~"),  # F9
+    76: _esc(b"[21~"),  # F10
+    95: _esc(b"[23~"),  # F11
+    96: _esc(b"[24~"),  # F12
 }
 
 ctrl_keys = {40: b"\x04", 54: b"\x03", 27: b"\x12"}  # D  # C  # R
@@ -81,88 +52,129 @@ colors = {
     "white": rl.WHITE,
 }
 
-ctrl = False
 
+class RayTerminal(pyte.HistoryScreen):
+    ctrl = False
 
-@ffi.callback("void(int,int,int,int)")
-def key_event(key, scancode, action, mods):
-    global ctrl
-    print("in-scancode=>", key, scancode, action, ctrl, mods)
-    if mods == 0:  # Key release
-        if action == 37:  # ctrl
-            print("ctrl-off")
-            ctrl = False
-        print("out-scancode=>", key, scancode, action, ctrl, mods)
-        return
+    def __init__(self, columns, rows):
+        super().__init__(columns, rows)
+        self.rows = rows
+        self._init_window()
+        self._spawn_shell()
+        rl.set_window_size(
+            int(self.text_size.x * columns), int(self.text_size.y * rows)
+        )
+        rl.set_target_fps(24)
 
-    # Key press or repeat
-    if action == 37:
-        print("ctrl-on")
-        ctrl = True
-    if ctrl and mods == 1:  # ctrl-key doesn't repeat
-        if data := ctrl_keys.get(action):
+    def _init_window(self):
+        rl.init_window(1, 1, b"CobraPy Terminal!")
+        font_path = str(
+            Path(rl.__file__).parent / "resources" / "fonts" / "monoid.ttf"
+        ).encode("utf-8")
+        self.font = rl.load_font_ex(font_path, 24, ffi.NULL, 0)
+        self.text_size = rl.measure_text_ex(self.font, b"X", self.font.baseSize, 0)
+
+        self.__cb = ffi.callback("void(int,int,int,int)")(lambda *a: self.key_event(*a))
+        rl.set_key_callback(self.__cb)
+
+    def _spawn_shell(self):
+        self.stream = pyte.ByteStream(self)
+        p_pid, master_fd = pty.fork()
+        if p_pid == 0:  # Child process
+            os.execvpe(
+                "bash",
+                ["bash"],
+                env=dict(
+                    TERM="vt220",
+                    COLUMNS=str(self.columns),
+                    LINES=str(self.rows),
+                    LC_ALL="en_US.UTF-8",
+                ),
+            )
+        self.p_out = os.fdopen(master_fd, "w+b", 0)
+
+    def key_event(self, key, scancode, action, mods):
+        print("in-scancode=>", key, scancode, action, self.ctrl, mods)
+        if mods == 0:  # Key release
+            if action == 37:  # ctrl
+                print("ctrl-off")
+                self.ctrl = False
+            print("out-scancode=>", key, scancode, action, self.ctrl, mods)
+            return
+
+        # Key press or repeat
+        if action == 37:
+            print("ctrl-on")
+            self.ctrl = True
+        if self.ctrl and mods == 1:  # ctrl-key doesn't repeat
+            if data := ctrl_keys.get(action):
+                print("--->", repr(data))
+                self.p_out.write(data)
+        elif data := keys.get(action):
             print("--->", repr(data))
-            p_out.write(data)
-    elif data := keys.get(action):
-        print("--->", repr(data))
-        p_out.write(data)
-    print("out-scancode=>", key, scancode, action, ctrl, mods)
+            self.p_out.write(data)
+        print("out-scancode=>", key, scancode, action, self.ctrl, mods)
 
+    def run(self):
+        while not rl.window_should_close():
+            # FIXME: can probably handle it all in key_event
+            key = rl.get_key_pressed()
+            while key > 0:
+                k = chr(key).encode("utf-8")
+                self.p_out.write(k)
+                key = rl.get_key_pressed()
 
-rl.set_key_callback(key_event)
+            rl.begin_drawing()
+            rl.clear_background(rl.DARKBLUE)
 
+            ready, _, _ = select.select([self.p_out], [], [], 0)
+            if ready:
+                try:
+                    data = self.p_out.read(1024)
+                    if data:
+                        self.stream.feed(data)
+                except OSError:  # Program went away
+                    break
 
-while not rl.window_should_close():
-    # FIXME: can probably handle it all in key_event
-    key = rl.get_key_pressed()
-    while key > 0:
-        k = chr(key).encode("utf-8")
-        p_out.write(k)
-        key = rl.get_key_pressed()
+            for y, line in enumerate(term.display):
+                line = term.buffer[y]
+                for x in range(term.columns):  # Can't enumerate, it's sparse
+                    char = line[x]
 
-    rl.begin_drawing()
-    rl.clear_background(rl.DARKBLUE)
+                    fg = colors.get(char.fg, rl.WHITE)
+                    bg = colors.get(char.bg, rl.BLACK)
+                    if char.reverse:
+                        fg, bg = bg, fg
 
-    ready, _, _ = select.select([p_out], [], [], 0)
-    if ready:
-        data = p_out.read(1024)
-        if data:
-            stream.feed(data)
+                    rl.draw_rectangle(
+                        int(x * self.text_size.x),
+                        int(y * self.text_size.y),
+                        int(self.text_size.x),
+                        int(self.text_size.y),
+                        bg,
+                    )
+                    rl.draw_text_ex(
+                        self.font,
+                        char.data.encode("utf-8"),
+                        (x * self.text_size.x, y * self.text_size.y),
+                        self.font.baseSize,
+                        0,
+                        fg,
+                    )
+            term.dirty.clear()
 
-    for y, line in enumerate(term.display):
-        line = term.buffer[y]
-        for x in range(term.columns):  # Can't enumerate, it's sparse
-            char = line[x]
-
-            fg = colors.get(char.fg, rl.WHITE)
-            bg = colors.get(char.bg, rl.BLACK)
-            if char.reverse:
-                fg, bg = bg, fg
-
+            # draw cursor
             rl.draw_rectangle(
-                int(x * text_size.x),
-                int(y * text_size.y),
-                int(text_size.x),
-                int(text_size.y),
-                bg,
+                int(term.cursor.x * self.text_size.x),
+                int(term.cursor.y * self.text_size.y),
+                int(self.text_size.x),
+                int(self.text_size.y),
+                (100, 108, 100, 100),
             )
-            rl.draw_text_ex(
-                font,
-                char.data.encode("utf-8"),
-                (x * text_size.x, y * text_size.y),
-                font.baseSize,
-                0,
-                fg,
-            )
-    term.dirty.clear()
+            rl.draw_fps(850, 10)
+            rl.end_drawing()
 
-    # draw cursor
-    rl.draw_rectangle(
-        int(term.cursor.x * text_size.x),
-        int(term.cursor.y * text_size.y),
-        int(text_size.x),
-        int(text_size.y),
-        (100, 108, 100, 100),
-    )
-    rl.draw_fps(850, 10)
-    rl.end_drawing()
+
+if __name__ == "__main__":
+    term = RayTerminal(80, 25)
+    term.run()
