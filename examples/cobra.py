be5b5dfe3229d2cea539778f1f08df3664afc50f
Roberto Alsina <roberto.alsina@gmail.com>
2020-10-14 13:31:01 -0300

    Skeleton of integrated environment (does NOT work)

---
+++ examples/cobra.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from cobra_py.graphics_server import Server
+from cobra_py.rl import Screen
+from cobra_py.terminal import Terminal
+
+
+class Cobrapy(Screen):
+    def __init__(self):
+        super().__init__(800, 600)
+        self.editor = Terminal(self, cmd="micro tasks.py")
+        self.term = Terminal(self, cmd="sweepleg")
+        self.graphics = Server(self)
+
+
+def main():
+    cobra = Cobrapy()
+    cobra.run()
+
+
+if __name__ == "__main__":
+    main()
--- examples/raylib_terminal.py
+++ examples/raylib_terminal.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
-from cobra_py.graphics_server import Server
 from cobra_py.rl import Screen
 from cobra_py.terminal import Terminal
 
 screen = Screen(800, 600)
-term = Terminal(screen, cmd="sweepleg")
-graphics = Server(screen)
+term = Terminal(screen, cmd="bash")
 screen.run()
